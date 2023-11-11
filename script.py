5c3da730be101d7d0c50f677ece1bf17a58c758c
madhukar123456 <shroudmg@gmail.com>
2023-11-11 08:59:49 +0545

    changes

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 import subprocess
-from multiprocessing import Process
-import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, Flask API is running!'
 
 def run_terminal_commands(commands):
     for command in commands:
@@ -9,11 +14,7 @@ def run_terminal_commands(commands):
             print(f'Successfully ran command: {command}')
         except subprocess.CalledProcessError as e:
             print(f'Error running command {command}: {e}')
-
-def run_flask_app():
-    from your_flask_app_module import app
-
-    app.run(debug=True)
+            
 
 if __name__ == "__main__":
     # List of terminal commands to run
@@ -25,21 +26,8 @@ if __name__ == "__main__":
         # Add more commands as needed
     ]
 
-     # Start the Flask API in a separate process
-    flask_process = Process(target=run_flask_app)
+    # Start running the terminal commands
+    run_terminal_commands(commands_to_run)
 
-    try:
-        # Start running the terminal commands
-        run_terminal_commands(commands_to_run)
-
-        # Start the Flask API
-        flask_process.start()
-
-        # Allow some time for the Flask API to start (adjust as needed)
-        time.sleep(2)
-
-    except KeyboardInterrupt:
-        pass
-    finally:
-        # Stop the Flask API process when done
-        flask_process.terminate()
+    # Start the Flask API
+    app.run(debug=True)
